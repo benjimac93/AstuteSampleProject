b0c46638803ed017444f469c5e6307efbad94223
Ben Macpherson <ben@uniquecomputing.com.au>
2020-11-24 14:17:21 +1100

    example code

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        readonly AstutepayrollcomWebServicev080 _api = new AstutepayrollcomWebServicev080();
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
